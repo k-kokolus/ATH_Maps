ae793a923976b341479829d08cdd76782a25beb1
VVoito <wojtek.kaczmarczyk@op.pl>
2017-01-17 12:41:43 +0100

    Kontury budynku

--- app/src/main/java/com/example/wojciech/myapplication/InteriorL.java
+++ app/src/main/java/com/example/wojciech/myapplication/InteriorL.java
@@ -1,53 +1,17 @@
 package com.example.wojciech.myapplication;
 
 import android.graphics.Color;
-import android.location.Geocoder;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.EditText;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+/**
+ * Created by Wojciech on 17.01.2017.
+ */
 
-import java.io.IOException;
-import java.util.List;
+public class InteriorL extends MapsActivity  {
 
-import static com.example.wojciech.myapplication.R.id.SearchBox;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    public GoogleMap mMap;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap)     //Pozycja (marker) startowa
-    {
-        mMap = googleMap;
-        LatLng start = new LatLng(49.7836529,19.0577128);
-        mMap.addMarker(new MarkerOptions().position(start).title("ATH"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(start));
-        mMap.setMinZoomPreference(18.5f);
-
-        //InteriorL inter = new InteriorL();
-       // inter.Draw_L_outside();
-
+    public void Draw_L_outside() {
         Polyline lineL_outside0 = mMap.addPolyline(new PolylineOptions()
                 .add(new LatLng(49.783415, 19.056886), new LatLng(49.783593, 19.057136))
                 .width(8)
@@ -112,29 +76,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .add(new LatLng(49.783415, 19.056886), new LatLng(49.783204, 19.057243))
                 .width(8)
                 .color(Color.DKGRAY));
-
-    }
-
-
-    public void onSearch(View view )    //Wyszukiwanie adresu
-    {
-        EditText location_tf = (EditText)findViewById(SearchBox);
-        String location = location_tf.getText().toString();
-        List<android.location.Address> addressList = null;
-
-        if (location != null || !location.equals(""))
-        {
-            Geocoder geocoder = new Geocoder(this);
-            try {
-                addressList = geocoder.getFromLocationName(location, 1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            android.location.Address address = addressList.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Znalazłem"));
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.setMinZoomPreference(18.5f);
-        }
     }
 }
